0185e5d4837cf8397a5f59581b4b3da0b6d7ddc9
Andres <abedandres@gmail.com>
2017-08-09 02:00:59 -0500

    Adds flash messages for import errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,15 @@ class UsersController < ApplicationController
   end
 
   def import
-    User.import(params[:csv])
-    redirect_to root_url, notice: "Users imported."
+    if params[:csv]
+      if params[:csv].content_type == "text/csv"
+        User.import(params[:csv])
+        redirect_to root_url, notice: "Users imported."
+      else
+        redirect_to root_url, notice: "Only csv files allowed"
+      end
+    else
+      redirect_to root_url, notice: "Please select a file."
+    end
   end
 end
